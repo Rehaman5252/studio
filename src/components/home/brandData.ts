91f0f850ef1705bd5e896673e49841f714ad5454
Rehaman Syed <rehaman@360-creations.com>
2025-07-12 12:33:48 +0000

    now lets add brand logos and format their represent Amazon - mixed forma

--- src/components/home/brandData.ts
+++ src/components/home/brandData.ts
@@ -10,10 +10,10 @@ export interface CubeBrand {
 }
 
 export const brands: CubeBrand[] = [
-    { id: 1, brand: 'Nike', format: 'WPL', logoUrl: '/logos/color/nike.svg', whiteLogoUrl: '/logos/white/nike.svg' },
-    { id: 2, brand: 'Apple', format: 'T20', logoUrl: '/logos/color/apple.svg', whiteLogoUrl: '/logos/white/apple.svg' },
-    { id: 3, brand: 'ICICI Bank', format: 'Mixed', logoUrl: '/logos/color/icici.svg', whiteLogoUrl: '/logos/white/icici.svg' },
-    { id: 4, brand: 'Gucci', format: 'Test', logoUrl: '/logos/color/gucci.svg', whiteLogoUrl: '/logos/white/gucci.svg' },
-    { id: 5, brand: 'Amazon', format: 'IPL', logoUrl: '/logos/color/amazon.svg', whiteLogoUrl: '/logos/white/amazon.svg' },
-    { id: 6, brand: 'Tata', format: 'ODI', logoUrl: '/logos/color/tata.svg', whiteLogoUrl: '/logos/white/tata.svg' },
+    { id: 1, brand: 'Amazon', format: 'Mixed', logoUrl: '/logos/color/amazon.svg', whiteLogoUrl: '/logos/white/amazon.svg' },
+    { id: 2, brand: 'Tata', format: 'IPL', logoUrl: '/logos/color/tata.svg', whiteLogoUrl: '/logos/white/tata.svg' },
+    { id: 3, brand: 'Nike', format: 'Test', logoUrl: '/logos/color/nike.svg', whiteLogoUrl: '/logos/white/nike.svg' },
+    { id: 4, brand: 'Apple', format: 'T20', logoUrl: '/logos/color/apple.svg', whiteLogoUrl: '/logos/white/apple.svg' },
+    { id: 5, brand: 'ICICI Bank', format: 'ODI', logoUrl: '/logos/color/icici.svg', whiteLogoUrl: '/logos/white/icici.svg' },
+    { id: 6, brand: 'Gucci', format: 'WPL', logoUrl: '/logos/color/gucci.svg', whiteLogoUrl: '/logos/white/gucci.svg' },
 ];
