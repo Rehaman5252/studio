4c9bb10c84fcbc54a0e8ff8ba3d789c8ddc824f5
Rehaman Syed <rehaman@360-creations.com>
2025-07-12 08:01:55 +0000

    havent updated yet

--- src/components/home/QuizSelection.tsx
+++ src/components/home/QuizSelection.tsx
@@ -10,7 +10,7 @@ import StartQuizButton from '@/components/home/StartQuizButton';
 
 const brands: CubeBrand[] = [
   { id: 1, brand: 'SBI', format: 'Test', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/State_Bank_of_India_logo.svg/1024px-State_Bank_of_India_logo.svg.png', logoWidth: 60, logoHeight: 60 },
-  { id: 2, brand: 'Gucci', format: 'Mixed', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/79/Gucci_logo.svg', logoWidth: 100, logoHeight: 60 },
+  { id: 2, brand: 'Gucci', format: 'Mixed', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Gucci_logo.svg/1024px-Gucci_logo.svg.png', logoWidth: 100, logoHeight: 60 },
   { id: 3, brand: 'Amazon', format: 'IPL', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg', logoWidth: 80, logoHeight: 50 },
   { id: 4, brand: 'PayPal', format: 'ODI', logoUrl: 'https://assets.stickpng.com/images/580b57fcd9996e24bc43c530.png', logoWidth: 80, logoHeight: 50 },
   { id: 5, brand: 'Nike', format: 'WPL', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg', logoWidth: 80, logoHeight: 40 },
@@ -51,7 +51,7 @@ function QuizSelection({ onStartQuiz }: QuizSelectionProps) {
         };
         
         if (timerRef.current) clearInterval(timerRef.current);
-        timerRef.current = setInterval(rotateToNextFace, 500);
+        timerRef.current = setInterval(rotateToNextFace, 3000);
         
         return () => {
             if (timerRef.current) clearInterval(timerRef.current);
@@ -68,6 +68,7 @@ function QuizSelection({ onStartQuiz }: QuizSelectionProps) {
             <QuizSelector 
                 brands={brands}
                 onFaceClick={handleCubeClick}
+                onRotation={handleRotation}
                 visibleFaceIndex={selectedBrandIndex}
             />
 
