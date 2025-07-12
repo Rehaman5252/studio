4f39921b27fe69cae3466b34a549ab4460a4288c
Rehaman Syed <rehaman@360-creations.com>
2025-07-12 11:47:35 +0000

    make the app more responsive fast and speed and very smoother

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from '@/context/Providers';
 const inter = Inter({ 
   subsets: ['latin'],
   variable: '--font-inter',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
