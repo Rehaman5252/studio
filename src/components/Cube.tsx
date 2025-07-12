ed503b1ee5a812e84256bd88a39b6616348cb8de
Rehaman Syed <rehaman@360-creations.com>
2025-07-12 11:43:40 +0000

    make the cube smaller boss and show the logos on the cube

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -13,12 +13,12 @@ interface CubeProps {
 }
 
 const faceTransforms = [
-  'rotateY(0deg) translateZ(125px)',      // Front
-  'rotateY(90deg) translateZ(125px)',     // Right
-  'rotateY(180deg) translateZ(125px)',    // Back
-  'rotateY(-90deg) translateZ(125px)',    // Left
-  'rotateX(90deg) translateZ(125px)',     // Top
-  'rotateX(-90deg) translateZ(125px)',    // Bottom
+  'rotateY(0deg) translateZ(75px)',      // Front
+  'rotateY(90deg) translateZ(75px)',     // Right
+  'rotateY(180deg) translateZ(75px)',    // Back
+  'rotateY(-90deg) translateZ(75px)',    // Left
+  'rotateX(90deg) translateZ(75px)',     // Top
+  'rotateX(-90deg) translateZ(75px)',    // Bottom
 ];
 
 const cubeRotation = (index: number) => {
@@ -39,16 +39,16 @@ export default function Cube({ brands, visibleFaceIndex, onFaceClick }: CubeProp
     };
 
     return (
-        <div className="w-full h-64 flex items-center justify-center perspective">
+        <div className="w-full h-48 flex items-center justify-center perspective">
             <div
-                className="relative w-56 h-56 preserve-3d transition-transform duration-1000"
+                className="relative w-40 h-40 preserve-3d transition-transform duration-1000"
                 style={rotationStyle}
             >
                 {brands.map((brand, index) => (
                     <div
                         key={brand.id}
                         className={cn(
-                            "absolute w-56 h-56 flex items-center justify-center p-4 cursor-pointer",
+                            "absolute w-40 h-40 flex items-center justify-center p-4 cursor-pointer",
                             "bg-card/50 border-2 border-primary/30",
                             "transition-all hover:border-primary hover:bg-primary/10"
                         )}
@@ -58,8 +58,8 @@ export default function Cube({ brands, visibleFaceIndex, onFaceClick }: CubeProp
                         <Image
                             src={brand.logoUrl}
                             alt={`${brand.brand} logo`}
-                            width={brand.logoWidth || 100}
-                            height={brand.logoHeight || 100}
+                            width={brand.logoWidth || 80}
+                            height={brand.logoHeight || 80}
                             className="object-contain"
                             style={{ filter: brand.invertOnDark ? 'invert(1)' : 'none' }}
                             priority
