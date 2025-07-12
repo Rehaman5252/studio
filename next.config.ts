c4a25e09d96d4dce492773b048452b65ad2ead0e
Rehaman Syed <rehaman@360-creations.com>
2025-07-12 07:00:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.icon-icons.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
