f8b970b676097bef89ea7e15a1b5c4660909e2f6
Rehaman Syed <rehaman@360-creations.com>
2025-07-13 10:44:11 +0000

    Make the following changes:
    On the page src/app/home/page.tsx,
    
    Find th

--- src/components/home/HomeClientContent.tsx
+++ src/components/home/HomeClientContent.tsx
@@ -37,7 +37,6 @@ export default function HomeClientContent() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="mt-10"
     >
       <QuizSelection />
     </motion.div>
