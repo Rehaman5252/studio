c6d9a3095d4cba7485aeea406d5c84ccbbd67f15
Rehaman Syed <rehaman@360-creations.com>
2025-07-13 03:58:46 +0000

    0
    I'm encountering a persistent TypeError: Cannot read properties of un

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 
 import { redirect } from 'next/navigation';
-import { getAuth } from 'firebase/auth';
-import { app } from '@/lib/firebase';
 
-// This is a server component.
-// NOTE: auth.currentUser on the server will ALWAYS be null with the client SDK.
-// The redirect logic in the /home page's client component will handle
-// redirecting unauthenticated users to /auth/login.
-// This page simply provides the fastest entry into the app.
-export default async function RootPage() {
+export default function RootPage() {
   redirect('/home');
 }
