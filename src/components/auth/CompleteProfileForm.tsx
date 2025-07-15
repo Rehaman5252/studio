d669ce6140867fa969b6ef80315e0b108982988d
Rehaman Syed <rehaman@360-creations.com>
2025-07-15 13:32:58 +0000

    remove all the changes happened in last 30 minutes. lets get back the ve

--- src/components/auth/CompleteProfileForm.tsx
+++ src/components/auth/CompleteProfileForm.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, 'useCallback', useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
