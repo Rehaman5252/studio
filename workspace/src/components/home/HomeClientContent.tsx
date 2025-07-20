57d86db4d4530daea138d74d162ce95eb3ca886a
Rehaman Syed <rehaman@360-creations.com>
2025-07-20 09:54:35 +0000

    starting build "17e529b8-c835-432f-8fac-93fb260209c6" FETCHSOURCE BUILD

--- workspace/src/components/home/HomeClientContent.tsx
+++ workspace/src/components/home/HomeClientContent.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useAuth } from '@/context/AuthProvider';
-import QuizSelection from './QuizSelection';
+import QuizSelection from '@/components/home/QuizSelection';
 import { Skeleton } from '@/components/ui/skeleton';
 
 const QuizSelectionSkeleton = () => (
