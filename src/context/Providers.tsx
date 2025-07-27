031023df1f9a7522ee4635fa284b12114e025478
Rehaman Syed <rehaman@360-creations.com>
2025-07-27 18:16:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -4,25 +4,21 @@
 import type { ReactNode } from 'react';
 import { UserDataProvider } from '@/context/AuthProvider';
 import { Toaster } from '@/components/ui/toaster';
-import { Suspense } from 'react';
-import { Loader2 } from 'lucide-react';
 import { QuizStatusProvider } from './QuizStatusProvider';
 import { FirebaseProvider } from '@/providers/FirebaseProvider';
 import { SettingsProvider } from '@/hooks/use-settings.tsx';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <Suspense fallback={<div className="flex h-screen w-screen items-center justify-center bg-background"><Loader2 className="h-12 w-12 animate-spin text-primary" /></div>}>
-        <FirebaseProvider>
-          <SettingsProvider>
-            <UserDataProvider>
-              <QuizStatusProvider>
-                {children}
-                <Toaster />
-              </QuizStatusProvider>
-            </UserDataProvider>
-          </SettingsProvider>
-        </FirebaseProvider>
-    </Suspense>
+    <FirebaseProvider>
+      <SettingsProvider>
+        <UserDataProvider>
+          <QuizStatusProvider>
+            {children}
+            <Toaster />
+          </QuizStatusProvider>
+        </UserDataProvider>
+      </SettingsProvider>
+    </FirebaseProvider>
   );
 }
