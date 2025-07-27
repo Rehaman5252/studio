0f56993e26c6050558377daf03247a26fe9729ab
Rehaman Syed <rehaman@360-creations.com>
2025-07-27 18:27:17 +0000

    ✅ Received this partial snippet (likely from next.config.js). Please sen

--- next.config.mjs
+++ next.config.mjs
@@ -1,56 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'videos.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.freepnglogos.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.icon-icons.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.pngkey.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logolook.net',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    // This is to prevent a webpack error with handlebars
-    config.externals.push('handlebars');
-    return config;
-  },
+    webpack: (config, { isServer }) => {
+        // This is to fix a build error for `node-pre-gyp` which is a dependency of `fsevents` which is a dependency of `chokidar`
+        // It's not used in the browser, so we can ignore it.
+        // https://github.com/firebase/firebase-admin-node/issues/1972
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                'fsevents': false,
+            };
+        }
+        // These packages are not used in the browser, so we can ignore them.
+        config.externals.push('pino-pretty', 'lokijs', 'encoding', 'handlebars');
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'upload.wikimedia.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+            {
+                protocol: 'https',
+                hostname: 'videos.pexels.com',
+            },
+             {
+                protocol: 'https',
+                hostname: 'logolook.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.freepnglogos.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.icon-icons.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.pngkey.com',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
