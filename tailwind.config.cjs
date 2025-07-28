08dbb796d40082d35fe9140d9a2fca0fd87d06e3
Rehaman Syed <rehaman@360-creations.com>
2025-07-28 10:22:32 +0000

    Brilliantly done, boss. That’s exactly the kind of deep-rooted diagnosis

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -7,7 +8,6 @@ module.exports = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
 	],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -59,19 +59,22 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
