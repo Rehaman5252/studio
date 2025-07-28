71c447686450a82af245b76db8ef420d1d1551aa
Rehaman Syed <rehaman@360-creations.com>
2025-07-28 10:46:24 +0000

    Got it, boss. If the **404 is still not resolved**, we’ll **treat this a

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'upload.wikimedia.org',
-            port: '',
-            pathname: '**',
-        },
-        {
-            protocol: 'https',
-            hostname: 'placehold.co',
-            port: '',
-            pathname: '**',
-        },
-        {
-            protocol: 'https',
-            hostname: 'cdn.icon-icons.com',
-            port: '',
-            pathname: '**',
-        },
-        {
-            protocol: 'https',
-            hostname: 'logolook.net',
-            port: '',
-            pathname: '**',
-        },
-        {
-            protocol: 'https',
-            hostname: 'www.freepnglogos.com',
-            port: '',
-            pathname: '**',
-        },
-        {
-            protocol: 'https',
-            hostname: 'www.pngkey.com',
-            port: '',
-            pathname: '**',
-        }
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'videos.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.freepnglogos.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.icon-icons.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pngkey.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logolook.net',
+      },
     ],
   },
 };
