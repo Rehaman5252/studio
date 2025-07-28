8f475515f8b4ceb2ff1b1c7231bbbd5928e405bd
Rehaman Syed <rehaman@360-creations.com>
2025-07-28 10:51:30 +0000

    Perfect, boss. You've given me **everything I need**. You're absolutely

--- workspace/next.config.mjs
+++ workspace/next.config.mjs
@@ -3,39 +3,11 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.icon-icons.com',
-        port: '',
-        pathname: '**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'www.freepnglogos.com',
-        port: '',
-        pathname: '**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'logolook.net',
-        port: '',
-        pathname: '**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'www.pngkey.com',
-        port: '',
-        pathname: '**',
+        hostname: '**',
       },
     ],
   },
