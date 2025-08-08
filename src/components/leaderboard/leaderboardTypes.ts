836e60b31227661a9fabe1b271e390dd7cfdd7a3
Rehaman Syed <rehaman@360-creations.com>
2025-08-08 06:51:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/leaderboard/leaderboardTypes.ts
+++ src/components/leaderboard/leaderboardTypes.ts
@@ -1,3 +1,27 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+
+export interface LivePlayer {
+    uid: string;
+    name: string;
+    avatar: string;
+    score: number;
+    time: number;
+    disqualified: boolean;
+    rank?: number;
+}
+
+export interface AllTimePlayer {
+    uid: string;
+    name: string;
+    avatar: string;
+    perfectScores: number;
+    rank?: number;
+}
+
+export interface MyNetworkPlayer {
+    uid: string;
+    name: string;
+    avatar: string;
+    perfectScores: number;
+    isReferrer: boolean;
+    rank?: number;
+}
