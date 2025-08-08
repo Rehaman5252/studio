0982870fef08ef2d27ac7aca8bbb5b1fe77fc6d2
Rehaman Syed <rehaman@360-creations.com>
2025-08-08 07:01:09 +0000

    You're currently facing multiple issues related to incorrect imports, Ta

--- src/components/home/brandData.ts
+++ src/components/home/brandData.ts
@@ -4,8 +4,7 @@ export interface CubeBrand {
     brand: string;
     format: string;
     logoUrl: string;
-    logoWidth: number;
-    logoHeight: number;
+    description: string;
 }
 
 export const brandData: CubeBrand[] = [
@@ -14,47 +13,41 @@ export const brandData: CubeBrand[] = [
         brand: 'Amazon',
         format: 'Mixed',
         logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg',
-        logoWidth: 100,
-        logoHeight: 30,
+        description: 'A mix of questions from all formats of cricket.'
     },
     {
         id: 2,
         brand: 'Mastercard',
         format: 'IPL',
         logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Mastercard_2019_logo.svg',
-        logoWidth: 80,
-        logoHeight: 80,
+        description: 'Test your knowledge on the Indian Premier League.'
     },
     {
         id: 3,
         brand: 'Netflix',
         format: 'T20',
         logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/1200px-Netflix_2015_N_logo.svg.png',
-        logoWidth: 40,
-        logoHeight: 40,
+        description: 'Fast-paced questions on T20 cricket.'
     },
     {
         id: 4,
         brand: 'ICICI',
         format: 'ODI',
         logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/12/ICICI_Bank_Logo.svg',
-        logoWidth: 90,
-        logoHeight: 50,
+        description: 'Challenge yourself with One Day International facts.'
     },
      {
         id: 5,
         brand: 'Gucci',
         format: 'WPL',
         logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Gucci_logo.svg',
-        logoWidth: 100,
-        logoHeight: 30,
+        description: 'Questions about the Women\'s Premier League.'
     },
     {
         id: 6,
         brand: 'Nike',
         format: 'Test',
         logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg',
-        logoWidth: 100,
-        logoHeight: 40,
+        description: 'Put your classic cricket knowledge to the test.'
     },
 ];
