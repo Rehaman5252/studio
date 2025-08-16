407579ffa95744f21c8ec763a8b52d066249e99d
Rehaman Syed <rehaman@360-creations.com>
2025-08-16 04:30:49 +0000

    Inside functions/index.js:
    const functions = require("firebase-function

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
-
-const { onRequest } = require('firebase-functions/v2/https');
-const { default: next } = require('next');
+const functions = require("firebase-functions/v2/https");
+const { default: next } = require("next");
 const path = require('path');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 // The standalone output is in .next/standalone/
+// So we need to go up one directory from `functions`
 const server = next({
   dev: isDev,
   conf: {
@@ -15,7 +15,7 @@ const server = next({
 
 const nextHandle = server.getRequestHandler();
 
-exports.nextServer = onRequest({ maxInstances: 2 }, async (req, res) => {
+exports.nextServer = functions.onRequest({ maxInstances: 2 }, async (req, res) => {
   await server.prepare();
   return nextHandle(req, res);
 });
