127ec87de917604729a5ec5b045aa0363421c541
Rehaman Syed <rehaman@360-creations.com>
2025-08-16 06:14:28 +0000

    ## How to Debug and Fix the 404 Error in Your Next.js CricBlitz Applicat

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone', // This is crucial for Firebase functions deployment
     images: {
         remotePatterns: [
             {
