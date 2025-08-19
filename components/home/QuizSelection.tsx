9093fdd949cbc23a47b16fc37f49ea793e792847
Rehaman Syed <rehaman@360-creations.com>
2025-08-19 10:17:38 +0000

    make cube 5 rems downwords

--- components/home/QuizSelection.tsx
+++ components/home/QuizSelection.tsx
@@ -155,7 +155,7 @@ const QuizSelectionComponent = () => {
                 <p className="text-sm text-muted-foreground">Click a face to select and play</p>
             </div>
             
-            <div className="flex justify-center items-center mt-14 mb-8 h-48 w-full">
+            <div className="flex justify-center items-center mt-20 mb-8 h-48 w-full">
                 <BrandCube onFaceClick={handleFaceClick} rotation={rotation} />
             </div>
 
