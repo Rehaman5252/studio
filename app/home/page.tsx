6daf06e01ccbcc3eea5fcd82c55a841f38a4a828
Rehaman Syed <rehaman@360-creations.com>
2025-08-19 09:58:39 +0000

    make the entire thing wider it means to use all the width of the space a

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -79,7 +79,7 @@ function HomePage() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
-            className="container mx-auto px-4 py-2"
+            className="px-4 py-2"
           >
             <MalpracticeWarning />
             <HomeClientContent />
