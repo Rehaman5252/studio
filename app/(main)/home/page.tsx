695863386fca80b87f6405de80b35f5e2df45a62
Rehaman Syed <rehaman@360-creations.com>
2025-08-21 13:47:02 +0000

    still unable to see header ? why like that ?

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -115,7 +115,7 @@ function HomePage() {
     };
 
     return (
-      <PageWrapper>
+      <PageWrapper title="indcric">
           <motion.div 
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
