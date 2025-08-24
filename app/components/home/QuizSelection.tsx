ed607ed6d518024450cbd741bc52afb63a7ac57d
Rehaman Syed <rehaman@360-creations.com>
2025-08-24 14:30:02 +0000

    when i select the format the cube stopped the rotation, banner stopper s

--- app/components/home/QuizSelection.tsx
+++ app/components/home/QuizSelection.tsx
@@ -84,14 +84,12 @@ const QuizSelectionComponent = ({ selectedBrand, setSelectedBrand, handleStartQu
     }, [isProfileComplete, handleStartQuiz]);
     
     const handleFaceClick = (brand: CubeBrand) => {
-        setIsRotating(false); 
         const clickedIndex = brandData.findIndex(b => b.id === brand.id);
         if (clickedIndex !== -1) {
             setCurrentFaceIndex(clickedIndex);
             setRotation(faceRotations[clickedIndex]);
             setSelectedBrand(brand);
         }
-        // No need for a timeout here, let initiateQuiz handle the logic
         initiateQuiz(brand);
     };
   
@@ -113,7 +111,10 @@ const QuizSelectionComponent = ({ selectedBrand, setSelectedBrand, handleStartQu
 
             <SelectedBrandCard 
                 selectedBrand={selectedBrand} 
-                onClick={() => initiateQuiz(selectedBrand)} 
+                onClick={() => {
+                    setIsRotating(false); 
+                    initiateQuiz(selectedBrand)}
+                } 
             />
 
             <div className="mt-6 space-y-8" id="tour-step-2">
