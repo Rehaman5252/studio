8768dbe24c9f322ef68a36cd71d365af3171c288
Rehaman Syed <rehaman@360-creations.com>
2025-08-27 07:50:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -30,6 +30,10 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'videos.pexels.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
             }
         ],
     },
