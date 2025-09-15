c3c81b0439b5db2e4b2fe93b0dd30f62fdfe9d82
Rehaman Syed <rehaman@360-creations.com>
2025-09-15 18:25:25 +0000

    the tagline should be "win indian rupees symbol 100 for every 100 second

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ function HomePage() {
           indcric
         </h1>
         <p className="mt-1 text-sm font-normal text-foreground/80">
-          The Ultimate Cricket Quiz Challenge!
+          win ₹100 for every 100 seconds!
         </p>
       </div>
     );
