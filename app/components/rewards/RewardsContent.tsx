8dfc4528d7b26beb1d55dcf7fc18d0026f61307b
Rehaman Syed <rehaman@360-creations.com>
2025-09-16 02:19:20 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- app/components/rewards/RewardsContent.tsx
+++ app/components/rewards/RewardsContent.tsx
@@ -14,7 +14,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 import Link from 'next/link';
 import { brandData } from '@/components/home/brandData';
 import { cn } from '@/lib/utils';
-import { normalizeTimestamp } from '@/lib/dates';
+import { normalizeTimestamp } from '@/app/lib/dates';
 
 const ScratchCardSkeleton = () => (
     <div className="w-full aspect-[4/5] p-1">
