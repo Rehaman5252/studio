f212f98a1a06b57694810f9c83d6fb09268b7d91
Rehaman Syed <rehaman@360-creations.com>
2025-09-18 10:09:30 +0000

    Perfect! Here's a complete summary of the final setup you should impleme

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,10 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: './jest-results', outputName: 'junit.xml' }],
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
