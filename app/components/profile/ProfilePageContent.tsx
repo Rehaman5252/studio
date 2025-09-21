9c072efb9efd453f7c950585abeac607026c9e4a
Rehaman Syed <rehaman@360-creations.com>
2025-09-21 04:39:26 +0000

    Exactly! Your plan is perfect. Refactoring `ProfilePageContent.tsx` to u

--- app/components/profile/ProfilePageContent.tsx
+++ app/components/profile/ProfilePageContent.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import dynamic from 'next/dynamic';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 import ProfileSkeleton from './ProfileSkeleton';
 import SupportCard from './SupportCard';
